f4c42ace2958ef2ce1402e100777519ebd0a8bd8
stnguswnd <stnguswnd@gmail.com>
2025-12-09 12:44:02 +0900

    jwt filter 작성

--- src/main/java/com/example/restapi/controller/TodoController.java
+++ src/main/java/com/example/restapi/controller/TodoController.java
@@ -12,6 +12,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 
 import java.util.List;
 
